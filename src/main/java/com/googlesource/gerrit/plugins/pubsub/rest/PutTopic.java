30f40578d855c0ecd2ac2e731ee806c26ef53b6e
Thomas Draebing <thomas.draebing@sap.com>
2023-02-17 09:48:17 +0100

    Add REST API endpoint to create PubSub topic for user
    This change provides Gerrit users with the possibility to create
    a Topic in PubSub. Only one Topic per user is allowed. The Topic
    name contains the account ID to make it identifiable and searchable.
    
    Change-Id: I15903fe22728fca7ba3ecdf546aea4e0910c8767

---
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/rest/PutTopic.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.pubsub.rest;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.rpc.AlreadyExistsException;
+import com.google.cloud.pubsub.v1.TopicAdminClient;
+import com.google.cloud.pubsub.v1.TopicAdminSettings;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.common.Input;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.pubsub.v1.Topic;
+import java.io.IOException;
+
+@Singleton
+public class PutTopic implements RestModifyView<AccountResource, Input> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final TopicAdminSettings topicAdminSettings;
+  private final PubsubTopicNameFactory topicNameFactory;
+
+  @Inject
+  public PutTopic(CredentialsProvider credentials, PubsubTopicNameFactory topicNameFactory)
+      throws IOException {
+    this.topicAdminSettings =
+        TopicAdminSettings.newBuilder().setCredentialsProvider(credentials).build();
+    this.topicNameFactory = topicNameFactory;
+  }
+
+  @Override
+  public Response<?> apply(AccountResource rsrc, Input input)
+      throws AuthException, BadRequestException, ResourceConflictException, IOException {
+    try (TopicAdminClient topicAdminClient = TopicAdminClient.create(topicAdminSettings)) {
+      Topic topic =
+          topicAdminClient.createTopic(
+              topicNameFactory.createForAccount(rsrc.getUser().getAccountId()));
+      logger.atInfo().log("Created pubsub topic: %s", topic.getName());
+    } catch (AlreadyExistsException e) {
+      return Response.none();
+    }
+    return Response.created();
+  }
+}
