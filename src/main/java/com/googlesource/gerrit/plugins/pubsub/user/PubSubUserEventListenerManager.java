c59f2e8e8cd12c6c95315740f9796becd7f0c6ed
Thomas Draebing <thomas.draebing@sap.com>
2023-03-01 13:36:53 +0100

    Publish user scoped events to PubSub
    If a new topic is craeted by a user, Gerrit will now also start
    publishing events that are visible to the user to the topic.
    
    On each start of Gerrit, all user-specific topics are looked up in
    GCP and the Publishers in Gerrit are created.
    
    Deletion of a topic and shutting down Gerrit will also gracefully
    stop the Publisher.
    
    Breaking change: The metrics for publishers were not containing the
    topic name, since only a single topic existed so far. Since this is
    not the case anymore, the topic name is now added to the
    corresponding metrics. This will require a change in the monitoring
    systems using those metrics.
    
    This is part of the effort to enable streaming events to end users
    using PubSub as an alternative to SSH stream-events.
    
    Bug: Issue 40015464
    Change-Id: I25cda8862ac78a0cfdd3231cd52df40ced3f0077

---
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PubSubUserEventListenerManager.java
@@ -0,0 +1,92 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.pubsub.user;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.inject.Inject;
+import com.google.pubsub.v1.ProjectName;
+import com.google.pubsub.v1.Topic;
+import com.googlesource.gerrit.plugins.pubsub.PubSubConfiguration;
+import com.googlesource.gerrit.plugins.pubsub.TopicProvider;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentMap;
+
+public class PubSubUserEventListenerManager implements LifecycleListener {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final ConcurrentMap<Account.Id, PubSubRegistrationHandle>
+      pubSubUserStreamEventListenerHandlers;
+  private final TopicProvider topicProvider;
+  private final String gcpProjectId;
+  private final PubSubUserTopicNameFactory topicNameFactory;
+  private final PubSubUserScopedEventListener.Factory userScopedEventListenerFactory;
+  private final IdentifiedUser.GenericFactory userFactory;
+
+  @Inject
+  public PubSubUserEventListenerManager(
+      @PubSubUserEventListenerHandlers
+          ConcurrentMap<Account.Id, PubSubRegistrationHandle> pubSubUserStreamEventListenerHandlers,
+      TopicProvider topicProvider,
+      PubSubConfiguration config,
+      PubSubUserTopicNameFactory topicNameFactory,
+      PubSubUserScopedEventListener.Factory userScopedEventListenerFactory,
+      IdentifiedUser.GenericFactory userFactory) {
+    this.pubSubUserStreamEventListenerHandlers = pubSubUserStreamEventListenerHandlers;
+    this.topicProvider = topicProvider;
+    this.gcpProjectId = config.getGCloudProject();
+    this.topicNameFactory = topicNameFactory;
+    this.userScopedEventListenerFactory = userScopedEventListenerFactory;
+    this.userFactory = userFactory;
+  }
+
+  @Override
+  public void start() {
+    registerExistingUserTopics();
+  }
+
+  @Override
+  public void stop() {
+    disconnectFromUserTopics();
+  }
+
+  private void registerExistingUserTopics() {
+    logger.atInfo().log("Registering existing PubSub EventListeners.");
+    try {
+      topicProvider
+          .list(ProjectName.of(gcpProjectId))
+          .iterateAll()
+          .forEach(this::registerUserTopic);
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log("Failed to list PubSub topics.");
+    }
+  }
+
+  private void registerUserTopic(Topic topic) {
+    Optional<Account.Id> optAccountId =
+        topicNameFactory.getAccountId(topic.getName().split("/")[3]);
+    if (optAccountId.isPresent()) {
+      logger.atInfo().log("Registering topic %s.", topic.getName());
+      userScopedEventListenerFactory.create(userFactory.create(optAccountId.get()));
+    }
+  }
+
+  private void disconnectFromUserTopics() {
+    pubSubUserStreamEventListenerHandlers.forEach((accountId, handle) -> handle.remove());
+  }
+}
