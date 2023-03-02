0a748daf85cbf64925065deb8be0e59625e308c6
Thomas Draebing <thomas.draebing@sap.com>
2023-03-02 09:35:29 +0100

    Allow users to create subscriptions to their topic
    Users can now create subscriptions to their topic. Multiple subscriptions
    can be created to allow multiple clients to subscribe to all events.
    
    The subscription name will contain the account ID and a random suffix. The
    subscription name will be returned by the request and can be used by the
    client to subscribe.
    
    Change-Id: I442c83794912449ff38b41a707a44fcaa77839ad

---
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PubSubUserSubNameFactory.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.pubsub.user;
+
+import com.google.gerrit.entities.Account;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.googlesource.gerrit.plugins.pubsub.PubSubConfiguration;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@Singleton
+public class PubSubUserSubNameFactory {
+  public static final String SUB_NAME_PREFIX = "stream-events-";
+
+  private final String gcpProjectId;
+
+  @Inject
+  public PubSubUserSubNameFactory(PubSubConfiguration config) {
+    this.gcpProjectId = config.getGCloudProject();
+  }
+
+  public ProjectSubscriptionName createForAccount(Account.Id accountId) {
+    StringBuilder subName = new StringBuilder();
+    subName.append(SUB_NAME_PREFIX);
+    subName.append(accountId.get());
+    subName.append("-");
+    subName.append(RandomStringUtils.random(6, true, true));
+    return ProjectSubscriptionName.of(gcpProjectId, subName.toString());
+  }
+}
