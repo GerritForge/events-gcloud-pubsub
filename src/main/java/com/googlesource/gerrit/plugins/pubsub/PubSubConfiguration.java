89f04e18a05e38eecacc2ca9b717abae5af07ac5
Thomas Draebing <thomas.draebing@sap.com>
2023-03-02 09:35:29 +0100

    Allow users to create subscriptions to their topic
    Users can now create subscriptions to their topic. Multiple subscriptions
    can be created to allow multiple clients to subscribe to all events.
    
    The subscription name will contain the account ID and a random suffix. The
    subscription name will be returned by the request and can be used by the
    client to subscribe.
    
    This is part of the effort to enable streaming events to end users
    using PubSub as an alternative to SSH stream-events. With this change
    the basic workflow supporting this scenario is complete:
    
    1. User creates topic
    2. Gerrit publishes events to the user's topic
    3. User creates subscription
    4. PubSub will push messages to user's client
    
    Bug: Issue 16842
    Change-Id: I442c83794912449ff38b41a707a44fcaa77839ad

--- src/main/java/com/googlesource/gerrit/plugins/pubsub/PubSubConfiguration.java
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/PubSubConfiguration.java
@@ -35,6 +35,7 @@ public class PubSubConfiguration {
   static final String SHUTDOWN_TIMEOUT_SECONDS_FIELD = "shutdownTimeoutInSeconds";
   static final String RETAIN_ACKED_MESSAGES_FIELD = "retainAckedMessages";
   static final String ENABLE_USER_STREAM_EVENTS_FIELD = "enableUserStreamEvents";
+  static final String USER_SUBSCRIPTION_SERVICE_ACCOUNT_FIELD = "serviceAccountForUserSubs";
 
   static final String DEFAULT_NUMBER_OF_SUBSCRIBERS = "6";
   static final String DEFAULT_ACK_DEADLINE_SECONDS = "10";
@@ -57,6 +58,7 @@ public class PubSubConfiguration {
   private final boolean sendStreamEvents;
   private final boolean retainAckedMessages;
   private final boolean enableUserStreamEvents;
+  private final String serviceAccountForUserSubs;
 
   @Inject
   public PubSubConfiguration(
@@ -89,6 +91,8 @@ public class PubSubConfiguration {
     this.enableUserStreamEvents =
         fromGerritConfig.getBoolean(
             ENABLE_USER_STREAM_EVENTS_FIELD, DEFAULT_ENABLE_USER_STREAM_EVENTS);
+    this.serviceAccountForUserSubs =
+        fromGerritConfig.getString(USER_SUBSCRIPTION_SERVICE_ACCOUNT_FIELD);
   }
 
   public String getGCloudProject() {
@@ -146,5 +150,9 @@ public class PubSubConfiguration {
 
   public boolean isEnableUserStreamEvents() {
     return enableUserStreamEvents;
+  }
+
+  public String getServiceAccountForUserSubs() {
+    return serviceAccountForUserSubs;
   }
 }
