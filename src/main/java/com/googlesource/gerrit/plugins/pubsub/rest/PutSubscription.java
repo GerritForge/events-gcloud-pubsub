36f2c8bb4f4542f2864f92eeec875bada90a9db7
Thomas Draebing <thomas.draebing@sap.com>
2023-03-02 09:35:29 +0100

    Allow users to create subscriptions to their topic
    Users can now create subscriptions to their topic. Multiple
    subscriptions can be created to allow multiple clients to
    subscribe to all events.
    
    The subscription name will contain the account ID and a
    random suffix. The subscription name will be returned by
    the request and can be used by the client to subscribe.
    
    This is part of the effort to enable streaming events to end
    users using PubSub as an alternative to SSH stream-events.
    With this change the basic workflow supporting this scenario
    is complete:
    
    1. User creates topic
    2. Gerrit publishes events to the user's topic
    3. User creates subscription
    4. PubSub will push messages to user's client
    
    Bug: Issue 16842
    Change-Id: I442c83794912449ff38b41a707a44fcaa77839ad

---
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/rest/PutSubscription.java
@@ -0,0 +1,73 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.pubsub.rest;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.pubsub.v1.Subscription;
+import com.googlesource.gerrit.plugins.pubsub.user.PubSubUserSubscriptionProvider;
+import com.googlesource.gerrit.plugins.pubsub.user.PubSubUserTopicNameFactory;
+import java.io.IOException;
+
+@Singleton
+@RequiresCapability(SubscribePubSubStreamEventsCapability.ID)
+public class PutSubscription extends PubSubRestModifyView<SubscriptionInput> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final PubSubUserSubscriptionProvider subscriptionProvider;
+  private final PubSubUserTopicNameFactory topicNameFactory;
+
+  @Inject
+  public PutSubscription(
+      PubSubUserSubscriptionProvider subscriptionProvider,
+      Provider<CurrentUser> userProvider,
+      PermissionBackend permissionBackend,
+      PubSubUserTopicNameFactory topicNameFactory) {
+    super(userProvider, permissionBackend);
+    this.subscriptionProvider = subscriptionProvider;
+    this.topicNameFactory = topicNameFactory;
+  }
+
+  @Override
+  public Response<SubscriptionInfo> applyImpl(AccountResource rsrc, SubscriptionInput input)
+      throws RestApiException {
+    if (input.pushEndpoint == null || input.pushEndpoint.isBlank()) {
+      throw new BadRequestException("Missing pushEndpoint URL.");
+    }
+    Account.Id accountId = rsrc.getUser().getAccountId();
+    try {
+      Subscription sub =
+          subscriptionProvider.getOrCreate(
+              topicNameFactory.createForAccount(accountId).getTopic(),
+              rsrc.getUser(),
+              input.pushEndpoint,
+              input.verificationToken);
+      return Response.created(SubscriptionInfo.create(sub, input.verificationToken));
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log("Failed to create subscription for account %s", accountId);
+      throw RestApiException.wrap("Failed to create subscription", e);
+    }
+  }
+}
--- src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PubSubUserEventListenerModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PubSubUserEventListenerModule.java
@@ -19,10 +19,19 @@ import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
+import com.googlesource.gerrit.plugins.pubsub.local.EnvironmentChecker;
+import com.googlesource.gerrit.plugins.pubsub.local.LocalPushSubscriptionEndpointUrlBuilder;
+import com.googlesource.gerrit.plugins.pubsub.local.LocalUserSubscriptionProvider;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class PubSubUserEventListenerModule extends AbstractModule {
+  private final EnvironmentChecker environmentChecker;
+
+  public PubSubUserEventListenerModule(EnvironmentChecker environmentChecker) {
+    this.environmentChecker = environmentChecker;
+  }
+
   @Override
   public void configure() {
     DynamicSet.bind(binder(), LifecycleListener.class).to(PubSubUserEventListenerManager.class);
@@ -32,5 +41,14 @@ public class PubSubUserEventListenerModule extends AbstractModule {
     bind(new TypeLiteral<ConcurrentMap<Account.Id, PubSubRegistrationHandle>>() {})
         .annotatedWith(PubSubUserEventListenerHandlers.class)
         .toInstance(new ConcurrentHashMap<Account.Id, PubSubRegistrationHandle>());
+
+    if (environmentChecker.isLocalEnvironment()) {
+      bind(PushSubscriptionEndpointUrlBuilder.class)
+          .to(LocalPushSubscriptionEndpointUrlBuilder.class);
+      bind(PubSubUserSubscriptionProvider.class).to(LocalUserSubscriptionProvider.class);
+    } else {
+      bind(PushSubscriptionEndpointUrlBuilder.class);
+      bind(PubSubUserSubscriptionProvider.class);
+    }
   }
 }
