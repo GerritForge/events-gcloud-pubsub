4e0c50b66432aeba053a761182dba79be0fbc9b5
Thomas Draebing <thomas.draebing@sap.com>
2023-04-04 09:48:31 +0200

    Allow to create push subscriptions to private endpoints
    Google PubSub does not support pushing messages to private endpoints
    (not reachable via the internet). Thus, out of the box clients
    within a company's intranet would not be able to subscribe to
    stream events.
    
    This issue can be solved by using a CloudRun service that proxies the
    request to the final endpoint. This works, because CloudRun can use
    a Serverless VPC Access connector to connect to a VPC.
    
    Users can now request a push subscription to a public or an internal
    endpoint, where pushes to the latter one will be proxied.
    
    A script and container for the proxy in CloudRun is also provided with
    this change.
    
    Bug: Issue 16842
    Change-Id: I31d6c2fabaa9cdad78d708c63dcaefd21ad7c1cf

---
+++ supplements/push-proxy/src/proxy.py
@@ -0,0 +1,84 @@
+import logging
+import os
+import sys
+
+import dns.resolver
+import requests
+
+from http.client import HTTPConnection
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+DEFAULT_NAMESERVERS = ["8.8.8.8", "4.4.4.4"]
+FORWARD_JWT = os.environ.get("FORWARD_JWT").lower() == "true"
+
+
+def _setup_logging():
+    if os.environ.get("DEBUG"):
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
+        HTTPConnection.debuglevel = 1
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def _setup_dns():
+    nameservers = os.environ.get("NAMESERVERS").split(",")
+    if len(nameservers) > 0:
+        nameservers.extend(DEFAULT_NAMESERVERS)
+        dns_resolver = dns.resolver.Resolver()
+        dns_resolver.nameservers = nameservers
+        dns.resolver.override_system_resolver(dns_resolver)
+
+
+def _get_auth_header(request, target_host):
+    if FORWARD_JWT:
+        return request.headers.get("Authorization")
+
+    response = requests.get(
+        f"http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/identity?audience={target_host}",
+        headers={"Metadata-Flavor": "Google"},
+    )
+    return f"Bearer {response.text}"
+
+
+@app.route("/", methods=["POST"])
+def index():
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        logging.error(msg)
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        logging.error(msg)
+        return f"Bad Request: {msg}", 400
+
+    logging.debug(f"Received event {envelope}")
+    host = request.args.get("host")
+    enduser_endpoint = f"https://{host}{request.args.get('path')}"
+    enduser_endpoint_with_token = (
+        f"{enduser_endpoint}?token={request.args.get('token')}"
+    )
+    logging.debug(f"Forwarding to host {enduser_endpoint_with_token}")
+
+    session = requests.Session()
+
+    headers = {"Authorization": _get_auth_header(request, enduser_endpoint)}
+    response = session.post(
+        enduser_endpoint_with_token,
+        json=request.get_json(),
+        headers=headers,
+        verify=False,
+    )
+
+    return ("", response.status_code)
+
+
+_setup_logging()
+_setup_dns()
