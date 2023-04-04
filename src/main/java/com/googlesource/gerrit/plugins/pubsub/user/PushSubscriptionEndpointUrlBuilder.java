e5049ad8bdc3c8e0ffe8d60c0d64cacfcf8c5530
Thomas Draebing <thomas.draebing@sap.com>
2023-04-04 09:48:31 +0200

    Allow to create push subscriptions to private endpoints
    Google PubSub does not support pushing messages to private endpoints
    (not reachable via the internet). Thus, out of the box clients
    within a company's intranet would not be able to subscribe to
    stream events.
    
    This issue can be solved by using a CloudRun service that proxies the
    request to the final endpoint. This works, because CloudRun can use
    a Serverless VPC Access connector to connect to a VPC.
    
    Users can now request a push subscription to a public or an internal
    endpoint, where pushes to the latter one will be proxied.
    
    A script and container for the proxy in CloudRun is also provided with
    this change.
    
    Bug: Issue 16842
    Change-Id: I31d6c2fabaa9cdad78d708c63dcaefd21ad7c1cf

--- src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PushSubscriptionEndpointUrlBuilder.java
+++ src/main/java/com/googlesource/gerrit/plugins/pubsub/user/PushSubscriptionEndpointUrlBuilder.java
@@ -14,27 +14,76 @@
 
 package com.googlesource.gerrit.plugins.pubsub.user;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.pubsub.PubSubConfiguration;
+
+@Singleton
 public class PushSubscriptionEndpointUrlBuilder {
+  private String userSubProxyEndpoint;
+
+  @Inject
+  public PushSubscriptionEndpointUrlBuilder(PubSubConfiguration pubSubProperties) {
+    this.userSubProxyEndpoint = pubSubProperties.getUserSubProxyEndpoint();
+  }
 
   public String getProtocol() {
     return "https";
   }
 
-  public String build(String pushEndPoint, String verificationToken) {
+  public String build(String pushEndpoint, String verificationToken) {
+    if (pushEndpoint == null || pushEndpoint.isBlank()) {
+      throw new IllegalArgumentException("Pushendpoint is required.");
+    }
     StringBuilder uri = new StringBuilder();
 
-    int protocolEnd = pushEndPoint.indexOf("://");
+    int protocolEnd = pushEndpoint.indexOf("://");
     if (protocolEnd > -1) {
-      if (!pushEndPoint.substring(0, protocolEnd).equals(getProtocol())) {
+      if (!pushEndpoint.substring(0, protocolEnd).equals(getProtocol())) {
         throw new IllegalArgumentException("Protocol has to be " + getProtocol());
       }
     } else {
       uri.append(getProtocol());
       uri.append("://");
     }
-    uri.append(pushEndPoint);
+    uri.append(pushEndpoint);
     if (verificationToken != null && !verificationToken.isBlank()) {
       uri.append("?token=");
+      uri.append(verificationToken);
+    }
+    return uri.toString();
+  }
+
+  public String buildProxied(String pushEndpoint, String verificationToken) {
+    if (userSubProxyEndpoint == null || userSubProxyEndpoint.isBlank()) {
+      throw new IllegalArgumentException("Can't push to internal network. Proxy URL not set.");
+    }
+    StringBuilder uri = new StringBuilder();
+    uri.append(userSubProxyEndpoint);
+    uri.append("?host=");
+
+    if (pushEndpoint == null || pushEndpoint.isBlank()) {
+      throw new IllegalArgumentException("Pushendpoint is required.");
+    }
+
+    int protocolEnd = pushEndpoint.indexOf("://");
+    if (protocolEnd > -1) {
+      if (!pushEndpoint.substring(0, protocolEnd).equals(getProtocol())) {
+        throw new IllegalArgumentException("Protocol has to be " + getProtocol());
+      }
+      pushEndpoint = pushEndpoint.substring(protocolEnd + 3);
+    }
+
+    String[] pushEndpointParts = pushEndpoint.split("/", 2);
+    uri.append(pushEndpointParts[0]);
+
+    if (pushEndpointParts.length == 2) {
+      uri.append("&path=/");
+      uri.append(pushEndpointParts[1]);
+    }
+
+    if (verificationToken != null && !verificationToken.isBlank()) {
+      uri.append("&token=");
       uri.append(verificationToken);
     }
     return uri.toString();
